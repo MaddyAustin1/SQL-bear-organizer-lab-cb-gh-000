7c12b3c1c1972f5219270ea33d4fc28e8262778f
Maddy Austin <maddy.austin@yahoo.co.uk>
2019-07-23 11:11:22 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,5 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT * FROM bears WHERE gender = \"F\" RETURN name and age;"
+  "SELECT * FROM bears WHERE gender = \"F\" GROUP BY name and age;"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
